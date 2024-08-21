689e54dd2acab55b85c785fba8b9fc9a7f7301bd
Alvin Manoj Alex <alvinmanoj02@gmail.com>
2024-08-21 08:44:36 -0400

    Code: Created landing page with firebase configs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { db } from './firebaseconfig';
+import { collection, addDoc } from 'firebase/firestore';
+
 import './App.css';
 
 function App() {
+  const [itemName, setItemName] = useState('');
+  const [tenantName, setTenantName] = useState('');
+  const [purchaseType, setPurchaseType] = useState('');
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+
+    if (!itemName || !tenantName || !purchaseType){
+      alert('Please fill all the details!');
+      return;
+    }
+
+    try {
+      const docRef = await addDoc(collection(db, 'shoppingList'), {
+        itemName,
+        tenantName,
+        purchaseType,
+        purchased: "no"
+      })
+
+      console.log('Document written');
+
+      setItemName('');
+      setTenantName('');
+      setPurchaseType('');
+    }
+    catch(error) {
+      console.log('Error adding document ', error);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className='container'>
+        <h1>B1 SHOPPING LIST</h1>
+
+        <input 
+          type='text'
+          placeholder='Enter item name'
+          value={itemName}
+          onChange={(e) => setItemName(e.target.value)} />
+
+        <select
+          value={tenantName}
+          onChange={(e) => setTenantName(e.target.value)}>
+            <option value=''>Select tenant</option>
+            <option value='Alvin'>Alvin</option>
+            <option value='Arnav'>Arnav</option>
+            <option value='Aryan'>Aryan</option>
+            <option value='Ishit'>Ishit</option>
+            <option value='Mohit'>Mohit</option>
+        </select>
+
+        <select
+          value={purchaseType}
+          onChange={(e) => setPurchaseType(e.target.value)}>
+            <option value=''>Select type</option>
+            <option value='personal'>Personal</option>
+            <option value='common'>Common for apartment</option>
+        </select>
+
+        <button className='submit-btn' onClick={handleSubmit} >
+          Submit Item
+        </button>
+      </div>
+
     </div>
   );
 }
