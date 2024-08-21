0df20f08f13b1b6129a98ccce43a3f122a8a3df0
Alvin Manoj Alex <alvinmanoj02@gmail.com>
2024-08-21 16:23:33 -0400

    Code: Seperated form and list components

--- src/App.js
+++ src/App.js
@@ -1,79 +1,21 @@
 import React, { useState } from 'react';
-import { db } from './firebaseconfig';
-import { collection, addDoc } from 'firebase/firestore';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import List from './List';
+import FormPage from './FormPage';
 
 import './App.css';
 
 function App() {
-  const [itemName, setItemName] = useState('');
-  const [tenantName, setTenantName] = useState('');
-  const [purchaseType, setPurchaseType] = useState('');
-
-  const handleSubmit = async(e) => {
-    e.preventDefault();
-
-    if (!itemName || !tenantName || !purchaseType){
-      alert('Please fill all the details!');
-      return;
-    }
-
-    try {
-      const docRef = await addDoc(collection(db, 'shoppingList'), {
-        itemName,
-        tenantName,
-        purchaseType,
-        purchased: "no"
-      })
-
-      console.log('Document written');
-
-      setItemName('');
-      setTenantName('');
-      setPurchaseType('');
-    }
-    catch(error) {
-      console.log('Error adding document ', error);
-    }
-  }
 
   return (
-    <div className="App">
-
-      <div className='container'>
-        <h1>B1 SHOPPING LIST</h1>
-
-        <input 
-          type='text'
-          placeholder='Enter item name'
-          value={itemName}
-          onChange={(e) => setItemName(e.target.value)} />
-
-        <select
-          value={tenantName}
-          onChange={(e) => setTenantName(e.target.value)}>
-            <option value=''>Select tenant</option>
-            <option value='Alvin'>Alvin</option>
-            <option value='Arnav'>Arnav</option>
-            <option value='Aryan'>Aryan</option>
-            <option value='Ishit'>Ishit</option>
-            <option value='Mohit'>Mohit</option>
-        </select>
-
-        <select
-          value={purchaseType}
-          onChange={(e) => setPurchaseType(e.target.value)}>
-            <option value=''>Select type</option>
-            <option value='personal'>Personal</option>
-            <option value='common'>Common for apartment</option>
-        </select>
-
-        <button className='submit-btn' onClick={handleSubmit} >
-          Submit Item
-        </button>
-      </div>
-
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<FormPage />} />
+        <Route path="/list" element={<List />} />
+      </Routes>
+    </Router>
   );
+
 }
 
 export default App;
---
+++ src/FormPage.js
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import { db } from './firebaseconfig';
+import { collection, addDoc } from 'firebase/firestore';
+import { Link } from 'react-router-dom';
+
+function FormPage() {
+    const [itemName, setItemName] = useState('');
+    const [tenantName, setTenantName] = useState('');
+    const [purchaseType, setPurchaseType] = useState('');
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+
+        if (!itemName || !tenantName || !purchaseType){
+            alert('Please fill all the details!');
+            return;
+            }
+
+            try {
+                const docRef = await addDoc(collection(db, 'shoppingList'), {
+                    itemName,
+                    tenantName,
+                    purchaseType,
+                    purchased: "no"
+                })
+
+                console.log('Document written');
+
+                setItemName('');
+                setTenantName('');
+                setPurchaseType('');
+            }
+            catch(error) {
+                console.log('Error adding document ', error);
+            }
+    }
+
+    return (
+        <div className="App">
+
+            <div className='container'>
+            <h1>B1 SHOPPING LIST</h1>
+
+            <input 
+                type='text'
+                placeholder='Enter item name'
+                value={itemName}
+                onChange={(e) => setItemName(e.target.value)} />
+
+            <select
+                value={tenantName}
+                onChange={(e) => setTenantName(e.target.value)}>
+                <option value=''>Select tenant</option>
+                <option value='Alvin'>Alvin</option>
+                <option value='Arnav'>Arnav</option>
+                <option value='Aryan'>Aryan</option>
+                <option value='Ishit'>Ishit</option>
+                <option value='Mohit'>Mohit</option>
+            </select>
+
+            <select
+                value={purchaseType}
+                onChange={(e) => setPurchaseType(e.target.value)}>
+                <option value=''>Select type</option>
+                <option value='personal'>Personal</option>
+                <option value='common'>Common for apartment</option>
+            </select>
+
+            <button className='submit-btn' onClick={handleSubmit} >
+                Submit Item
+            </button>
+
+            <Link to='/list'>
+                <button className='submit-btn'>View List</button>
+            </Link>
+            </div>
+
+        </div>
+  )
+}
+
+export default FormPage;
